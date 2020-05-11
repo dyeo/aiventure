8c4127a95f88d0fd3a0ab22d6251516cf5f54a8c
Dan-Home <dan@dyeo.net>
2020-05-11 18:14:32 -0400

    Add ability to load previous saves to MenuScreen

--- aiventure/uix/menu.py
+++ aiventure/uix/menu.py
@@ -1,4 +1,5 @@
 import os
+import json
 import threading
 
 from kivy.logger import Logger
@@ -43,27 +44,48 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         if super(SelectableLabel, self).on_touch_down(touch):
             return True
         if self.collide_point(*touch.pos) and self.selectable:
-            return self.parent.select_with_touch(self.index, touch)
-
+            return self.parent.select_with_touch(self.index, touch)  
+      
     def apply_selection(self, rv, index, is_selected):
         ''' Respond to the selection of items in the view. '''
+        pass
+
+
+class SelectableModelLabel(SelectableLabel):
+    def apply_selection(self, rv, index, is_selected):
         is_reselected = self.selected == is_selected
         self.selected = is_selected
         if is_selected and not is_reselected:
             self.screen.on_model_selected(self.parent.parent.data[index]['text'])
+
+
+class SelectableGameLabel(SelectableLabel):
+    def apply_selection(self, rv, index, is_selected):
+        is_reselected = self.selected == is_selected
+        self.selected = is_selected
+        if is_selected and not is_reselected:
+            self.screen.on_game_selected(self.parent.parent.data[index]['text'])
+
 
 class MenuScreen(Screen):
     
     def __init__(self, **kw):
         super().__init__(**kw)
         self.app = App.get_running_app()
+        self.savefiles = {}
+        self.selected_savefile = None
 
     def on_enter(self):
         self.app.adventure = Adventure()
-        self.ids.view_model.data = [{'text': str(m)} for m in self.get_module_directories()]
+        self.init_models()
+        self.init_saves()
         self.update_button_start_new()
+        self.update_button_start_load()
     
     # AI MODEL TAB
+
+    def init_models(self):        
+        self.ids.view_model.data = [{'text': str(m)} for m in self.get_module_directories()]
 
     def get_module_directories(self) -> list:
         return [m.name for m in os.scandir(self.app.get_user_path('models')) if self.model_is_valid(m.path)]
@@ -93,6 +115,11 @@ class MenuScreen(Screen):
             self.ids.label_model.text = f'Error Loading Model "{model_name}"'
         else:
             self.ids.label_model.text = f'Loaded Model: {model_name} ({self.app.generator.ai.model_info})'
+            self.on_update()
+
+    def on_update(self):
+        self.update_button_start_new()
+        self.update_button_start_load()
 
     # NEW GAME TAB
 
@@ -113,4 +140,33 @@ class MenuScreen(Screen):
         else:
             self.ids.button_start_new.text = 'Please Load Model to Start'
             self.ids.button_start_new.disabled = True
+    
+    # LOAD GAME TAB
+    
+    def init_saves(self):
+        paths = [s.path for s in os.scandir(self.app.get_user_path('adventures')) if s.path.endswith('.json')]
+        for p in paths:
+            with open(p, 'r') as json_file:
+                data = json.load(json_file)
+                self.savefiles[data['name']] = data        
+        self.ids.view_game.data = [{'text': str(s)} for s in self.savefiles.keys()]
+
+    def on_game_selected(self, game):
+        self.selected_savefile = game
+
+    def on_start_load(self):
+        self.app.adventure.from_dict(self.savefiles[self.selected_savefile])
+        self.app.sm.current = 'play'
+
+    def update_button_start_load(self):        
+        if self.app.generator:
+            if self.selected_savefile:
+                self.ids.button_start_load.text = 'Start Adventure'
+                self.ids.button_start_load.disabled = False
+            else:
+                self.ids.button_start_load.text = 'Select a Save File'
+                self.ids.button_start_load.disabled = True
+        else:
+            self.ids.button_start_load.text = 'Please Load Model to Start'
+            self.ids.button_start_load.disabled = True
 
